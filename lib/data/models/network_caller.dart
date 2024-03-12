44968adbbd99e23ee19781972b3362921e92d89e
fokrul511 <islamfokrul2002@gmail.com>
2024-03-12 10:02:29 +0600

    update

---
+++ lib/data/models/network_caller.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:task_manager/data/service/response_object.dart';
+import 'package:http/http.dart';
+
+class NetworkCaller {
+  static Future<ResponseObject> getRequest(String url) async {
+    try {
+      final Response response = await get(Uri.parse(url));
+      log(response.statusCode.toString());
+      log(response.body.toString());
+      if (response.statusCode == 200) {
+        final decodeResponse = jsonDecode(response.body);
+        return ResponseObject(
+            statusCode: 200, responsBody: decodeResponse, isSuccess: true);
+      } else {
+        return ResponseObject(
+            statusCode: response.statusCode, responsBody: "", isSuccess: false);
+      }
+    } catch (e) {
+      log(e.toString());
+      return ResponseObject(
+          isSuccess: false,
+          statusCode: -1,
+          responsBody: "",
+          errorMessage: e.toString());
+    }
+  }
+
+  static Future<ResponseObject> postRequest(
+      String url, Map<String, dynamic> body) async {
+    try {
+      final Response response = await post(
+        Uri.parse(url),
+        body: jsonEncode(body),
+        headers: {'content-type': "Application/json"},
+      );
+      log(response.statusCode.toString());
+      log(response.body.toString());
+      if (response.statusCode == 200) {
+        final decodeResponse = jsonDecode(response.body);
+        return ResponseObject(
+            statusCode: 200, responsBody: decodeResponse, isSuccess: true);
+      } else {
+        return ResponseObject(
+            statusCode: response.statusCode, responsBody: "", isSuccess: false);
+      }
+    } catch (e) {
+      log(e.toString());
+      return ResponseObject(
+          isSuccess: false,
+          statusCode: -1,
+          responsBody: "",
+          errorMessage: e.toString());
+    }
+  }
+}
