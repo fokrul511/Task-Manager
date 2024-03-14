c5d99155d9d6cbee2e82b162ba67b97f14e2705f
fokrul511 <islamfokrul2002@gmail.com>
2024-03-14 16:13:14 +0600

    update

--- lib/presentation/screens/new_task_screen.dart
+++ lib/presentation/screens/new_task_screen.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:task_manager/presentation/widgets/background_widget.dart';
 import 'package:task_manager/presentation/widgets/profile_app_bar.dart';
+import 'package:task_manager/presentation/widgets/task_counter_card.dart';
 
 class NewTaskScreen extends StatefulWidget {
   const NewTaskScreen({super.key});
@@ -15,25 +17,30 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
     return Scaffold(
       appBar: profileAppBar,
       body: BackgroundWidget(
-        child: const Column(
+        child: Column(
           children: [
-            Card(
-              child: Column(
-                children: [
-                  Text(
-                    '12',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text("New"),
-                ],
+            SizedBox(
+              height: 110,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemCount: 6,
+                itemBuilder: (context, index) {
+                  return const NewTaskCard(
+                    amount: 12,
+                    title: "New",
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return const SizedBox(
+                    width: 8,
+                  );
+                },
               ),
-            )
+            ),
           ],
         ),
       ),
     );
   }
 }
+
